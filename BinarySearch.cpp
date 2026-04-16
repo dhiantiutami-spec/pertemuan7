f2ff45cf3396d9e7fd6ee76cbc29f32d7d4c3b75
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-04-16 10:31:52 +0700

    menambahkan looping(for)

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -27,5 +27,9 @@ void input()
     cout << "     Masukkan Elemen Array      ";
     cout << "==============================\n";
 
-    
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i]
+    }
 }
